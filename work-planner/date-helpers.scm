c8bb6f714e7811aea61d927bb664694dceb6969d
James Crake-Merani <crake-meranij@cardiff.ac.uk>
2022-11-06 14:17:38 +0000

    Created function for comparing dates.
    It checks whether two dates fall on the same calendar day.

---
+++ work-planner/date-helpers.scm
@@ -0,0 +1,24 @@
+;; Work Planner
+;; Copyright (C) 2022 James Crake-Merani
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+(define-module (work-planner date-helpers))
+(use-modules (srfi srfi-19))
+
+(define (same-day? date1 date2)
+  (and
+   (= (date-day date1) (date-day date2))
+   (= (date-month date1) (date-month date2))
+   (= (date-year date1) (date-year date2))))
