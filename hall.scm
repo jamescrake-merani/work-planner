e2eb8cd864b9f59db6a94031e678ab704dbbbe6f
James Crake-Merani <crake-meranij@cardiff.ac.uk>
2022-11-11 17:40:43 +0000

    Updated scan.

--- hall.scm
+++ hall.scm
@@ -6,21 +6,33 @@
   (copyright (2022))
   (synopsis "")
   (description "A tool for managing your work.")
-  (home-page "https://github.com/jamescrake-merani/work-planner")
+  (home-page
+    "https://github.com/jamescrake-merani/work-planner")
   (license glp3+)
   (dependencies `())
   (skip ())
   (files (libraries
-           ((scheme-file "work-planner")
-            (directory "work-planner" ())))
+           ((directory
+              "work-planner"
+              ((scheme-file "date-json")
+               (scheme-file "filters")
+               (scheme-file "date-helpers")))))
          (tests ((directory "tests" ())))
          (programs ((directory "scripts" ())))
          (documentation
            ((org-file "README")
             (symlink "README" "README.org")
-            (text-file "HACKING")
             (text-file "COPYING")
-            (directory "doc" ((texi-file "work-planner")))))
+            (directory
+              "doc"
+              ((texi-file "version")
+               (info-file "work-planer")
+               (texi-file "work-planner")
+               (text-file "stamp-vti")
+               (text-file ".dirstamp")))
+            (text-file "NEWS")
+            (text-file "AUTHORS")
+            (text-file "ChangeLog")))
          (infrastructure
            ((scheme-file "guix")
             (text-file ".gitignore")
