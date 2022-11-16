14aeb3861debfc84d36166651977d95a309f3f5e
James Crake-Merani <crake-meranij@cardiff.ac.uk>
2022-11-16 12:01:20 +0000

    Show due date if requested.

--- work-planner/command-line.scm
+++ work-planner/command-line.scm
@@ -15,8 +15,18 @@
 ;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 (define-module (work-planner command-line))
-(use-modules (work-planner date-json))
+(use-modules (work-planner date-json)
+             (srfi srfi-19))
 
 ;; This function is going to be extended a lot.
-(define (work-item-string-representation item)
-  (work-item-text item))
+(define* (work-item-string-representation item
+                                          #:key show-due-date
+                                          (date-format "~d/~m/~y ~T"))
+  (let ((date-str
+         (if show-due-date
+             (string-append
+              (date->string (work-item-due-date item) date-format)
+              ": ")
+             "")))
+    (string-append date-str (work-item-text item))))
+(export work-item-string-representation)
