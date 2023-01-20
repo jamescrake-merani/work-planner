287b6555b56d6bc83a20e57ebfe0376b36a504aa
James Crake-Merani <crake-meranij@cardiff.ac.uk>
2023-01-20 19:17:59 +0000

    Past date should not be inclusive on purgable.

--- work-planner/filters.scm
+++ work-planner/filters.scm
@@ -61,5 +61,5 @@
 (define* (make-filter-purgable #:optional (today (current-date)))
   (lambda (item)
     (let ((due-date (work-item-due-date item)))
-      (not (and (work-item-completed? item) (or (not due-date) (past-date? today due-date)))))))
+      (not (and (work-item-completed? item) (or (not due-date) (past-date? today due-date #f)))))))
 (export make-filter-purgable)
